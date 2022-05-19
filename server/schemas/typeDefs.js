c962cbc9e4c40f419e10b3d495331b60a440b7a6
Franklin <franklin.c.brown@gmail.com>
2022-05-19 10:37:20 -0700

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    author: [String] // this is an array of strings?
+    description: String
+    title: String
+    image: Int // need to know how to use an image
+    link: String // is this correct?
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    book(bookId: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(author: [String!]!, description: String!, title: String!, bookId, ID!, image: link!, link: String!): Thought
+    addReaction(thoughtID: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
